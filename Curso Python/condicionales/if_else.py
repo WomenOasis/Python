f136fecc4acf30c4520f7d022661b63f618099af
womenoasis <leidybeatrizvegaarguello@gmail.com>
2024-02-19 09:53:58 -0500

    practica de condicionales con python

---
+++ Curso Python/condicionales/if_else.py
@@ -0,0 +1,15 @@
+edad = 17
+
+if edad > 18:
+    print("puede pasar")
+else:
+    print("no puede pasar")
+    
+
+contraseña_almacenada = "Ladyvega"
+contraseña_escrita = "Ladyvega"
+
+if contraseña_almacenada == contraseña_escrita:
+    print("Iniciando sesion...")
+else :
+    print("Contraseña incorrecta, Intente de nuevo") 
