1ecd13ff4266ced37d9c59f568241f44b6f4855f
WomenOasis <leidybeatrizvegaarguello@gmail.com>
2024-02-21 22:15:10 -0500

    ok2

--- Curso Python/Metodo/metodo_cadena.py
+++ Curso Python/Metodo/metodo_cadena.py
@@ -15,4 +15,20 @@ minus = cadena1.lower()
 #primer letra mayuscula
 primera_letra_mayus = cadena1.capitalize()
 
-print(primera_letra_mayus )
+#buscamos una cadena en otra cadena, si no hay concidencias devuelve -1
+busqueda_find = cadena1.find("hola soy women ") 
+
+#buscamos una cadena en otra cadena, si no hay conicidencias lanza una excepcion error
+busqueda_index = cadena1.index("a")
+
+#si es numerico, devolvemos true, sino devolvems falsee
+es_numerico = cadena1.isnumeric()
+
+#si es alfanumerico devolvemos true, sino devolvemos true
+es_alfanumerico = cadena1.isalpha()
+
+#buscamos una cadena en otra cadena, devuelve la cantidad de veces que coincida 
+contar_coincidencias = cadena1.count("o")
+
+print(contar_coincidencias)
+
