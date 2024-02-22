0aa0e4ce0e6b9af7d4635c73645eb65b133a3958
womenoasis <leidybeatrizvegaarguello@gmail.com>
2024-02-22 18:24:46 -0500

    feat: :sparkles: metodos
    listas
    
    by:lady vega

--- Curso Python/Metodo/metodo_cadena.py
+++ Curso Python/Metodo/metodo_cadena.py
@@ -1,4 +1,4 @@
-cadena1 = "hola soy women"
+cadena1 = "hola, soy women"
 Cadena2 = "Bienvenidos oasis"
 
 
@@ -28,7 +28,24 @@ es_numerico = cadena1.isnumeric()
 es_alfanumerico = cadena1.isalpha()
 
 #buscamos una cadena en otra cadena, devuelve la cantidad de veces que coincida 
-contar_coincidencias = cadena1.count("o")
+contar_coincidencias = cadena1.count("a")
 
-print(contar_coincidencias)
+#contar cuantos caracteres tiene una cadena (cuantas letras)
+contar_caracteres = len(cadena1)
 
+#verficamos si una cadena empieza con otra cadena dada, si es asi devuelve true
+empieza_con = cadena1.startswith("hola")
+
+#verficamos si una cadena termina con otra cadena dada, si es asi devuelve true
+termina_con = cadena1.endswith("en")
+
+#si el valor 1, se encuentra en la cadena original, reemplaza el valor 1 de la misma, por el valor 2
+# reemplaza un pedazo por la cadena dada, por otra dada
+cadena_nueva = cadena1.replace("women", "oasis")
+cadena_nueva_2 = cadena_nueva.capitalize()
+
+#separar cadena con la cadena que le pasemos (es una lista)
+cadena_separada = cadena1.split(",") 
+
+
+print(type(cadena_separada))
