30eddaa3866ebf585c846214f31f00b54738d545
womenoasis <leidybeatrizvegaarguello@gmail.com>
2024-02-23 09:36:35 -0500

    feat: :sparkles: metodo_diccionario
    termine los metodos de diccionario
    
    by:lady vega

--- Curso Python/Metodo/metodo_diccionario.py
+++ Curso Python/Metodo/metodo_diccionario.py
@@ -1,4 +1,4 @@
-#se puede como letras y numeros (nombre o 0)
+#se puede como letras y numeros (nombre o 0) 
 diccionario = {
     "nombre" : "women",
     "apellido" : "oasis",
@@ -11,5 +11,15 @@ claves1 = diccionario.keys()
 #get es un metodo que puede acceder una propiedad particular de un objeto (obteniendo un elemento con ge()) (si no encuentra nada el programa continua)
 claves2 = diccionario.get("nombre")
 
+#liminando todo del diccionario 
+#diccionario.clear()
 
-print(claves2)
+#eliminando un elemento de un diccionario
+diccionario.pop("apellido")
+
+#obteniendo un elemento dict_items iterable}
+diccionario_iterable = diccionario.items()
+
+
+
+print(diccionario_iterable)
