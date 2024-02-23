2a53c2d1b92fdf711558b6411cbc1096a0504142
womenoasis <leidybeatrizvegaarguello@gmail.com>
2024-02-23 10:25:23 -0500

    feat: :sparkles: input_nombre
    como pedirle al usuario su nombre
    
    by:lady vega

---
+++ Curso Python/inputs/input_nombres.py
@@ -0,0 +1,10 @@
+#pedirle un dato al usuario }
+nombre = input("Programadora, dame tu nombre: ")
+
+#mostrando el dato ejm 1
+print(nombre)
+
+#mostrando el dato ejm 2
+print(f'el nombre es: {nombre}')
+
+
---
+++ Curso Python/inputs/input_numeros.py
@@ -0,0 +1,13 @@
+#pedir un numero al usuario
+numero = input("dame un numero, programador: ")
+
+#convertimos los dos numeros a enteros y se suma o si es decimal float y etc.
+#convierto el numero a entero y lo multiplico por 2
+#resultado1 =  int(numero) * 2
+
+#convierto el numero flotante y lo multiplico por 2
+resultado_flotante = float(numero) * 2
+
+#mostrando el resultado 
+print(resultado2)
+
