983ebcf09d0d2b6671c277d18c7f4f2418753b5e
womenoasis <leidybeatrizvegaarguello@gmail.com>
2024-02-29 17:37:05 -0500

    feat: :sparkles: Diccionario
    aprendiendo
    
    by:lady vega

--- Curso Python/variable2.0/diccionarios.py
+++ Curso Python/variable2.0/diccionarios.py
@@ -4,11 +4,13 @@ diccionario = dict(nombre="lucas",apellido="dalto")
 #las listas no pueden ser claves porque son mutables y usamos frozenset para meter conjuntos
 diccionario = {frozenset(["dalto", "rancio"]): "jajaja"}
 
-#creando diccionario con fromkeys()
-diccionario = dict.fromkeys(["nombre", "apellido", "suscriptores"])
+#creando diccionario con fromkeys() valor por defecto none
+diccionario1 = dict.fromkeys(["nombre", "apellido"])
+
+#creando diccionario con fromkeys() con dos parametros 
+diccionario2 = dict.fromkeys(["nombre", "apellido"], "No se")
+diccionario3 = dict.fromkeys("ABCD", "Algun valor fijo")
 
 
 
-
-
-print(diccionario)
+print(diccionario2)
