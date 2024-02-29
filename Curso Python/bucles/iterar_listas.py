32b60fcd812f5d39176b09f7dd85519ec5c051e4
womenoasis <leidybeatrizvegaarguello@gmail.com>
2024-02-29 17:36:35 -0500

    feat: :sparkles: iteras_listas
    aprendiendo
    
    by:lady vega

---
+++ Curso Python/bucles/iterar_listas.py
@@ -0,0 +1,11 @@
+animales = ["gato","perro","loro","cocodrilo"]
+numeros = [10,62,12,72]
+ 
+
+
+for animal in animales:
+    print(f'Ahora la variable animal es igual a: {animal}')
+    
+    
+    
+ 
