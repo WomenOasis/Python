6d046c9560bc73a5e5822d5da11fa38d412d5595
womenoasis <leidybeatrizvegaarguello@gmail.com>
2024-02-29 17:36:04 -0500

    feat: :sparkles: Proyecto python
    aprendiendo menus
    
    by:lady vega

---
+++ Reto_python/Productos,py/Prod.py
@@ -0,0 +1,45 @@
+#Menu diccionario tenemos tres categorias y cada categorias tiene 10 productos
+menu = dict({
+    "Skincare":({
+        "Productos":list([
+            {"nombre": "1 - Protector solar", "valor": 12000},
+            {"nombre": "2 - Contorno de ojeras", "valor": 10000},
+            {"nombre": "3 - Sobre para puntos negros", "valor": 3000},
+            {"nombre": "4 - Pomos de maquillaje", "valor": 7000},
+            {"nombre": "4 - Mascarilla nocturna", "valor": 18000},
+            {"nombre": "6 - Macarilla de aguacate", "valor": 18000},
+            {"nombre": "7 - Colageno para labios", "valor": 20000},
+            {"nombre": "8 - Colageno para ojeras", "valor": 20000},
+            {"nombre": "9 - Serum gotero de vitamina C", "valor": 10800},
+            {"nombre": "10 - Desmaquillante", "valor": 25000}, 
+]),
+    "Promociones":list([
+        {"codigo": 4, "nombre": "dompra de 2", "valor: ":12000 },
+        {"codigo": 8, "nombre": "dompra de 2", "valor: ":18000 }    
+    ])      
+    }),
+    
+    "Pollos_vip":{
+       "Productos":list([
+           {"nombre": "1 - Pollo al carbon", "Valor": 100000 },
+           {"nombre": "2 - hamburguesa de pollo", "Valor": 25000},
+           {"nombre": "3 - jugo de fresa", "Valor": 8000},
+           {"nombre": "4 - pizza de pollo", "Valor": 15000},
+           {"nombre": "5 - alitas BBQ", "Valor": 13900},
+           {"nombre": "6 - 6 piezas de pollo Broaster", "Valor": 17000}
+           {"nombre": "7 - Hamburgueza pollo apenado", }
+           {}
+           {}
+           {}
+           
+           
+           
+           
+           
+           
+           
+       ])
+        
+
+    }
+})
