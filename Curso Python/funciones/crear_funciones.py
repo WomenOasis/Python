184a952e90e8ea489ac0963356c35e40abcece38
WomenOasis <leidybeatrizvegaarguello@gmail.com>
2024-03-02 20:19:11 -0500

    feat: :sparkles: crear funciones
    by:lady vega

--- Curso Python/funciones/crear_funciones.py
+++ Curso Python/funciones/crear_funciones.py
@@ -1,2 +1,32 @@
- 
- 
+#creando una funcion simple
+def saludar():
+    print("hola leidy, influencer¿Como estas?")
+
+#ejecutando la funcion simple 
+saludar()
+
+
+#Crear una funcion que tenga parametros
+def saludar(nombre,sexo):
+    sexo.lower()
+    if(sexo == "mujer"):
+        adjetivo = "reina"
+    elif(awxo == "hombre"):
+        adejtivo = "titan"
+    else:
+        adejtivo = "amor"
+    print(f"Hola {nombre}, mi {adjetivo},¿Como estas?")
+
+saludar("leidy", "mujer")         
+saludar("pipe", "hombre")
+saludar("fran", "no binario")
+
+#crear una funcion que nos retorne valores
+def ccrear_contraseña_random(num):
+    listado_De_caracteres = ["abcdf"]
+    num_entero = str(num)
+    c1 = num_entero - 2
+    c2 = num_entero 
+    c3 = num_entero - 5
+    num = int(num_entero[num_entero])
+    num * 2
